52854aaeb2b3cf0d4d4f802cb383b5bd5db7fc1f
xguu9604 <xguu9604@gmail.com>
2022-11-20 21:31:24 +0900

    1120_change_account_view

--- back_pjt/accounts/views.py
+++ back_pjt/accounts/views.py
@@ -3,12 +3,4 @@ from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 
-def login(request):
-    if request.method == 'POST':
-        pass
-    else:
-        form = AuthenticationForm()
-    context = {
-        'form': form
-    }
-    return render(request, 'accounts/login.html', context)
+    
