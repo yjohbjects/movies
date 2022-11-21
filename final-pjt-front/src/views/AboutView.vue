22df9606e7f58930f249e01bbc097d9eb261b016
yjohbjects <yjohbjects@gmail.com>
2022-11-22 00:10:40 +0900

    YJ: review related pages created

--- final-pjt-front/src/views/AboutView.vue
+++ final-pjt-front/src/views/AboutView.vue
@@ -1,26 +1,32 @@
 <template>
   <div class="about">
 
-
     <!-- 집으로 가는 방법 두가지 -->
     <h1>This is an about page</h1>
     <button @click="toHome">to home</button>
     <router-link :to="{ name: 'Home' }">to home</router-link><br>
 
-   <img src="https://pds.joins.com/news/component/htmlphoto_mmdata/202007/13/1cf33940-bf50-410b-8ac2-c0dd9cd31955.jpg" 
-   width="30%" height="30%"
-   style="mouseoverStyle"
+    <div style="width:218px; height:327px; background-color: black;opacity: 0.3 ; background-image: url(https://pds.joins.com/news/component/htmlphoto_mmdata/202007/13/1cf33940-bf50-410b-8ac2-c0dd9cd31955.jpg)">
+      <h5>제목</h5>
+      <p>장르</p>
+      <br>
+      <p>평점 남기기</p>
+      <star-rating
+        v-bind:increment="0.5"
+        v-bind:max-rating="5"
+        inactive-color="#808080"
+        active-color="#FED000"
+        v-bind:star-size="30">
+
+      </star-rating>        
+      <button>나중에 볼 영화</button>
+
+    </div>
+
+   <img style="width:218px; height:327px;" src="https://pds.joins.com/news/component/htmlphoto_mmdata/202007/13/1cf33940-bf50-410b-8ac2-c0dd9cd31955.jpg" 
    @mouseover="shadowedImage"
    @click="zoomedImage">
 
-   <star-rating
-     v-bind:increment="0.5"
-     v-bind:max-rating="5"
-     inactive-color="#000"
-     active-color="#cc1166"
-     v-bind:star-size="30">
-
-   </star-rating>        
 
   </div>
 </template>
@@ -32,6 +38,11 @@ export default {
   name: 'AboutView',
   components: {
     StarRating
+  },
+  data() {
+    return {
+      isMouseover: false,
+    }
   },
   methods: {
     toHome() {
