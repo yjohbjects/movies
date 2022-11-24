3fcaabf27a348b172fd08745e388431ad86e62dd
yjohbjects <yjohbjects@gmail.com>
2022-11-24 23:48:36 +0900

    YJ: 찐찐막

--- final-pjt-front/src/components/ReviewDetailCard.vue
+++ final-pjt-front/src/components/ReviewDetailCard.vue
@@ -18,7 +18,11 @@ export default {
   },
   methods: {
     toReviewDetail() {
-      this.$router.push({ name: 'ReviewDetail', params: { reviewId: this.review.id } })
+      if(this.$store.state.user === this.review.review_user["id"]) {
+        this.$router.push({ name: 'ReviewDetail', params: { reviewId: this.review.id } })
+      } else {
+        alert('작성한 유저만 수정이 가능합니다.')
+      }
     }
   }
 }
